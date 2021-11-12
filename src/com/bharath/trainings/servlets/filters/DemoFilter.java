b0ae7316a61dc16f38021f6e59baa7a3efdb9931
swaticraghav <swaticraghav@gmail.com>
2021-11-12 15:51:34 +0530

    first commit

---
+++ src/com/bharath/trainings/servlets/filters/DemoFilter.java
@@ -0,0 +1,53 @@
+package com.bharath.trainings.servlets.filters;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class DemoFilter
+ */
+@WebFilter("/FilterDemoServlet")
+public class DemoFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public DemoFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("Pre Servlet. </br>");
+		chain.doFilter(request, response);
+		out.println("Post Servlet");
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
